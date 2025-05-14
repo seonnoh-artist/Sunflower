53c0da404ee9c098e4e073fd9a121b9f5aa69e82
seonnoh-artist <seonnoh.heart@gmail.com>
2025-05-14 21:45:34 +0900

    수정

--- app.js
+++ app.js
@@ -310,7 +310,7 @@ function draw() {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('./service-worker.js')
     .then(function (registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     })
--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/libraries/p5.min.js',
-  '/libraries/p5.sound.min.js',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-  '/data/shootingStar_bk.jpg',
-  '/data/star0.png',
-  '/data/star1.png',
-  '/data/star2.png',
-  '/data/star3.png',
-  '/data/star4.png'
+  './',
+  './index.html',
+  './app.js',
+  './libraries/p5.min.js',
+  './libraries/p5.sound.min.js',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './data/shootingStar_bk.jpg',
+  './data/star0.png',
+  './data/star1.png',
+  './data/star2.png',
+  './data/star3.png',
+  './data/star4.png'
 ];
 
 self.addEventListener('install', function (event) {
