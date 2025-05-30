c85aee8334843b1f81c6b4296adfb17e7ad27ac1
seonnoh-artist <seonnoh.heart@gmail.com>
2025-05-30 16:52:10 +0900

    시간수정

--- app.js
+++ app.js
@@ -116,8 +116,8 @@ function printLog() {
   text(log_str, 100, 100);
   let volume_str = mic.getLevel();
   text("vol: " + nf(volume_str, 1, 6), 100, 300);
-  text("satrtHour: " + startHour, 100, 330);
-  text("endHour: " + endHour, 100, 360);
+  text("satrtHour: " + startHour, 100, 200);
+  text("endHour: " + endHour, 100, 250);
 }
 
 // ==================== 작품 초기화 ====================
